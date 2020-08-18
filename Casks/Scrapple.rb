33f6a1384506ff99833d46ab88fa1f7c5a2429d2
Linus S <LinusS1@users.noreply.github.com>
2020-08-17 18:45:44 -0700

    Add Scrapple Cask

---
+++ Casks/Scrapple.rb
@@ -0,0 +1,12 @@
+cask 'Scrapple' do
+	version '1.0.0'
+	sha256 '62e3e92752bcf2139101a06da739efd39cf2e7f032bb604d3f3d49ebd89851df'
+	
+	url "https://github.com/LinusS1/Scrapple/releases/download/v#{version}/Scrapple.zip"
+	name 'Scrapple'
+	homepage 'https://github.com/LinusS1/Scrapple'
+	appcast 'https://Scrapple.pythonanywhere.com/static/appcast.xml'
+
+	auto_updates true
+	app 'Scrapple.app'
+end
