fb443bacb3a582a17cc7672278b97f60d24552a3
Linus S <LinusS1@users.noreply.github.com>
2020-08-29 14:48:24 -0700

    Update scrapple.rb

--- Casks/scrapple.rb
+++ Casks/scrapple.rb
@@ -1,6 +1,6 @@
 cask 'scrapple' do
-	version '1.1'
-	sha256 'f945894f435edcc33eb00360059c1d78239b648645ca3e145c780b79a223fe9b'
+	version '2.0'
+	sha256 '8e29e9ff1465db86f6b063de755284cab41ddc67c43fe9e31a38ae591afc3cb6'
 	
 	url "https://github.com/LinusS1/Scrapple/releases/download/v#{version}/Scrapple.zip"
 	name 'Scrapple'
